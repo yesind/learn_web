e2718e957cf487a114da5076845666415e238559
Yesin Dmitry <yesinds@gmail.com>
2022-10-08 21:08:56 +0300

    First commit

---
+++ webapp/python_org_news.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
+
+
+def get_python_news():
+    html = get_html("https://www.python.org/blogs/")
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        news_list = soup.find('ul', class_='list-recent-posts').findAll('li')
+        result_news = []
+        for news in news_list:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published = news.find('time').text
+            result_news.append({
+                'title': title,
+                'url': url,
+                'published': published
+            })
+        return result_news
+    return False    
+
+
+
