3904bb1948f631cab7d1cb07de8a8a785aa86d2d
Yesin Dmitry <yesinds@gmail.com>
2022-10-08 22:59:24 +0300

    Add Database

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -21,13 +23,18 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published
-            })
-        return result_news
-    return False    
+            try:
+                published = datetime.strptime(published, '%b. %d, %YYYY')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title=title, url=url, published=published)
+            
 
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
 
 
